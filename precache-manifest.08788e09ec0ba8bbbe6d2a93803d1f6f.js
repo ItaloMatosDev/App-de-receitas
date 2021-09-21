f51b7cbea943f9f8bd7a43efe08835c75254770f
Italo <italomatos764@gmail.com>
2021-09-20 21:46:40 -0400

    Updates

---
+++ precache-manifest.08788e09ec0ba8bbbe6d2a93803d1f6f.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "12985ecafdf259dbb253fac830033ddc",
+    "url": "/App-de-receitas/index.html"
+  },
+  {
+    "revision": "6e553bab0884b134b0df",
+    "url": "/App-de-receitas/static/css/main.3571927e.chunk.css"
+  },
+  {
+    "revision": "d9097d85b20326404d70",
+    "url": "/App-de-receitas/static/js/2.b2fab287.chunk.js"
+  },
+  {
+    "revision": "737926aa5f30acaedb866e79ac0007c9",
+    "url": "/App-de-receitas/static/js/2.b2fab287.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6e553bab0884b134b0df",
+    "url": "/App-de-receitas/static/js/main.f04ffc99.chunk.js"
+  },
+  {
+    "revision": "c021187531b89bb8630a",
+    "url": "/App-de-receitas/static/js/runtime-main.81debe0a.js"
+  },
+  {
+    "revision": "86a2f13e91ac85080ebaeaab9463b9f1",
+    "url": "/App-de-receitas/static/media/Poppins-ExtraLight.86a2f13e.ttf"
+  },
+  {
+    "revision": "55bf161260cf59c43fca28fffc2d8850",
+    "url": "/App-de-receitas/static/media/drinksExplore.55bf1612.jpg"
+  },
+  {
+    "revision": "a2b9c189659341ec679f1dda010de7fc",
+    "url": "/App-de-receitas/static/media/ingredients3.a2b9c189.jpg"
+  },
+  {
+    "revision": "d12c22aa544df22ac455dd202730439f",
+    "url": "/App-de-receitas/static/media/mealsExplore.d12c22aa.jpeg"
+  },
+  {
+    "revision": "203e2941c66c8cc9ba6c15be1a9e4a71",
+    "url": "/App-de-receitas/static/media/people2.203e2941.jpg"
+  },
+  {
+    "revision": "1f5edec942f922b8333b05e47259b315",
+    "url": "/App-de-receitas/static/media/profile-image.1f5edec9.jpg"
+  },
+  {
+    "revision": "74e487e504d5cf16d7832bef2fbacfea",
+    "url": "/App-de-receitas/static/media/profileIcon.74e487e5.svg"
+  },
+  {
+    "revision": "db81213596cb3beb56440bb39aa70c8c",
+    "url": "/App-de-receitas/static/media/searchIcon.db812135.svg"
+  },
+  {
+    "revision": "0c0cd5e20d91357ca0b8d6bc6b0a5e82",
+    "url": "/App-de-receitas/static/media/world.0c0cd5e2.jpg"
+  }
+]);
